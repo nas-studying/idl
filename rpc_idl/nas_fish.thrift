f6d6a5ce82b6e0a48aa1ebedf24dab008fb1b53b
Li EnZhao <119293210+NasLez@users.noreply.github.com>
2025-12-29 11:34:59 +0800

    Add nas_fish.thrift for SignUp service definition

---
+++ rpc_idl/nas_fish.thrift
@@ -0,0 +1,13 @@
+namespace go nas_fish
+
+struct SignUpReq {
+    1: required string email
+}
+
+struct SignUpResp {
+    1: required i64 id
+}
+
+service NasFish{
+    SignUpResp SignUp(1: SignUpReq req)
+}
